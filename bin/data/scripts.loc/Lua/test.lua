82695d3b9bc89caab52fe864dcfacd7d84e42dc3
Senpai <senpai@thunderspy.net>
2026-09-06 08:53:15 -0500

    Remove data from this source code repo
    The data included in this repo wasn't comprehensive or necessary to be
    included. It should be provided from elsewhere.

--- bin/data/scripts.loc/Lua/test.lua
+++
@@ -1,8 +0,0 @@
-player = Entity.MyEntity()
-
-doppel = Entity.CreateDoppelganger(nil, "nopowersangel", Entity.GetCombatLevel(player), "Citadel?", player, player, nil, "Elite", "Citadillyo", false)
-
-
-EndScript()
-
-function tick() end
